fe907c5b2a3af3b9ae83d42f9d26b815a398294d
anywn <996234342@qq.com>
2024-12-10 14:52:02 +0800

    Add Yuku as DeAI Manifesto signee

--- src/DeAIManifesto_frontend/src/helpers/organization_signees.js
+++ src/DeAIManifesto_frontend/src/helpers/organization_signees.js
@@ -56,6 +56,13 @@ export const organizationSignees = [
     url: "https://formyfi.io/",
     signDate: "2024-12-04",
     signedBy: "Azizbek Khalilov",
+  },
+  {
+    name: "Yuku",
+    logo: "./yuku.jpeg",
+    url: "https://yuku.app/",
+    signDate: "2024-12-10",
+    signedBy: "",
   }
 ];
   
